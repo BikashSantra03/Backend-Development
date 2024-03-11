73342f52ff47acb4fec035d0757b5373e5a12f88
Bikash Santra <89884891+BikashSantra03@users.noreply.github.com>
2024-03-11 10:42:33 +0530

    Update server.js

--- Backend-Auth/server.js
+++ Backend-Auth/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 require("colors");
+
 require("dotenv").config();
 
 const { connectDB } = require("./config/db");
@@ -8,15 +10,17 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 const PORT = process.env.PORT || 8000;
 
 //mounting
-const userRoutes = require("./routes/userRoutes.js")
+const userRoutes = require("./routes/userRoutes.js");
 app.use("/api/v1", userRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("<h1>This is Homepage</h1>")
-})
+app.get("/", (req, res) => {
+  res.send("<h1>This is Homepage</h1>");
+});
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`.brightBlue.bold);
 });
