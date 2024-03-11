689617d31992b7bf5ade22b1750ede19e6799abe
Bikash Santra <89884891+BikashSantra03@users.noreply.github.com>
2024-03-11 10:43:20 +0530

    Update userRoutes.js

--- Backend-Auth/routes/userRoutes.js
+++ Backend-Auth/routes/userRoutes.js
@@ -1,42 +1,54 @@
-
-const {loginController,signUpController} = require("../controllers/auth")
-const{auth,isAdmin,isStudent}= require("../middlewares/authMiddelware")
-const express = require("express")
+const { loginController, signUpController } = require("../controllers/auth");
+const { auth, isAdmin, isStudent } = require("../middlewares/authMiddelware");
+const userModel = require("../models/userSchema");
+const express = require("express");
 
 const router = express.Router();
 
 //routes
-
-router.get("/login",loginController)
-router.post("/Signup",signUpController)
-
-
-
+router.get("/login", loginController);
+router.post("/Signup", signUpController);
 
 //testing protected routes for single middleware
-router.get("/test", auth, (req,res) =>{
-    res.json({
-        success:true,
-        message:'Welcome to the Protected route for TESTS',
-    });
+router.get("/test", auth, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for TESTS",
+  });
 });
-
-
 
 //protected Routes
-router.get("/student", auth, isStudent, (req,res) => {
-    res.json({
-        success:true,
-        message:'Welcome to the Protected route for Students',
-    });
-} );
-
-router.get("/admin", auth, isAdmin, (req,res) => {
-    res.json({
-        success:true,
-        message:'Welcome to the Protected route for Admin',
-    });
+router.get("/student", auth, isStudent, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for Students",
+  });
 });
 
+router.get("/admin", auth, isAdmin, (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome to the Protected route for Admin",
+  });
+});
 
-module.exports= router;
+router.get("/getEmail", auth, async (req, res) => {
+  try {
+    const id = req.user.id;
+    const existingUser = await userModel.findById(id); //findOne({_id:id})
+    res.status(200).json({
+      success: true,
+      mail: existingUser.email,
+      user: existingUser,
+      message: "Welcome to the email route",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      error: error.message,
+      message: "Error while fetching Email!",
+    });
+  }
+});
+
+module.exports = router;
